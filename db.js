bdef7585f2555c84554f8c49299ebc8a1dcd08c4
Shanna Ostermiller <shannaostermiller@gmail.com>
2021-11-13 13:49:58 -0500

    update db file

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
-const { Sequelize } = require("sequelize");
+const {
+  Sequelize
+} = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
+  }
+}
 })
-
-
 module.exports = db;
