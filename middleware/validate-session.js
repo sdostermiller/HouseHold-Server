b857fc99f88371a4af25af71df6ea8213331788e
Shanna Ostermiller <shannaostermiller@gmail.com>
2021-11-13 16:50:49 -0500

    again

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -37,40 +37,33 @@
 // };
 
 // module.exports = validateSession;
-const jwt = require("jsonwebtoken")
-const { UserModel } = require("../models")
-const validateSession = async (req, res, next) => {
-  if (req.method == "OPTIONS") {
-    next();
-  } else if (
-    req.headers.authorization &&
-    req.headers.authorization.includes("Bearer")
-  ) {
-    const { authorization } = req.headers
-    // console.log("authorization -->,", authorization)
-    const payload = authorization
-      ? jwt.verify(
-        authorization.includes("Bearer")
-          ? authorization.split(" ")[1]
-          : authorization,
-          process.env.JWT_SECRET
-      )
-      : undefined;
-      // console.log('payload -->", payload')
-      if (payload) {
-        // let foundUser = await UserModel.findOne({ where: { id: payload.id } })
-        let foundUser = await UserModel.findOne({ where: {id: payload.id } });
-        // console.log("foundUser -->", foundUser)
-        if (foundUser) {
-          req.user = foundUser;
-          next()} else {
-            res.status(400).send({ message: "Not Authorized" })
-          }
-        } else {
-          res.status(401).send({ message: "INvalid token" })
-        }
-    } else {
-      res.status(403).send({ message: "Forbidden" })
-    }
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+
+const validateSession = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(403).send({ auth: false, message: "No Token Provided" });
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+      if (!err && decodeToken) {
+        User.findOne({
+          where: {
+            id: decodeToken.id,
+          },
+        })
+          .then((user) => {
+            if (!user) throw err;
+            req.user = user;
+            return next();
+          })
+          .catch((err) => next(err));
+      } else {
+        req.errors = err;
+        return res.status(500).send("Not Authorized");
+      }
+    });
   }
-  module.exports = validateSession
+};
+module.exports = validateSession;
