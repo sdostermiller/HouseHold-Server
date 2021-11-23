aa48d77669d2448c4503868b8d53db1f019d7b04
Shanna Ostermiller <shannaostermiller@gmail.com>
2021-11-22 20:09:16 -0500

    fix cors AGAIN :|

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,6 @@
 const CorsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "POST, PUT, GET, DELETE, OPTIONS");
+  res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "POST, PUT, GET, DELETE, OPTIONS");
   res.header("access-control-allow-headers", "Origin, X-Request-With, Content-Type, Accept, Authorization");
 
   
